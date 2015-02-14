f17b00ef11e9b313f2b0cc3d9a7c0c4ee262f9a3
Sohrab Salehi <sohrab@219-dhcp175.stat.ubc.ca>
2015-02-13 18:51:21 -0800

    * Added functions to generate CTMC rate matrix and state space for before and after the occurance of the rate event. Warning: the driver function has to be updated.

--- r.scripts/generalized_dollo_simulator.R
+++ r.scripts/generalized_dollo_simulator.R
@@ -15,6 +15,11 @@
   # TODO: fix the problem with the unrooted tree
   # TODO: assume there is no complete delete
   # TODO: incorporate b to the state space
+  # TODO: the above process shouldn't be able to delete
+  # TODO: make it compatible with are state space
+  # TODO: add the emission model
+  # TODO: add distribution test on the nucleotide on each leaf at each site chaning the root
+  # TODO: add test for above and below processs
   
   # Inputs
   states <- NULL
@@ -22,6 +27,7 @@
   Q <- NULL
   pi <- NULL
   runAbove <- TRUE
+  runBelow <- TRUE
   shallPrint <- TRUE
   
   ## CTMC
@@ -34,22 +40,22 @@
   
   # Simulate wait time (get the corresponding row to the current state i, simulate with exp(-Q(i, i)),   )
   # maybe collect states
-  sampleCTMC <- function(init.state, duration) {
+  sampleCTMC <- function(init.state, duration, rateMatrix) {
     time.passed <- 0
     current.state <- init.state
     pprint(current.state)
     # absorbing state
-    if (Q[current.state, current.state] == 0) return(current.state)
+    if (rateMatrix[current.state, current.state] == 0) return(current.state)
         
     while (time.passed < duration) {
       # simulate wait time
-      wait.time <- rexp(1, -Q[current.state, current.state])
+      wait.time <- rexp(1, -rateMatrix[current.state, current.state])
       
       # choose a new state (shouldn't be the same one)
-      current.state <- sample(states[states != current.state], 1, prob = Q[current.state, -c(current.state)])
+      current.state <- sample(states[states != current.state], 1, prob = rateMatrix[current.state, -c(current.state)])
       
       # absorbing state
-      if (Q[current.state, current.state] == 0) return(current.state)
+      if (rateMatrix[current.state, current.state] == 0) return(current.state)
       
       # update time passed
       time.passed <- time.passed + wait.time
@@ -86,7 +92,7 @@
     
     pprint(final.index)
     for (i in edge.index:final.index) {
-      result[tree$edge[i,2]] <- sampleCTMC(result[[tree$edge[i,1]]], tree$edge.length[i])
+      result[tree$edge[i,2]] <- sampleCTMC(result[[tree$edge[i,1]]], tree$edge.length[i], Q)
     }
     
     # change null with length(states) + 1
@@ -101,6 +107,7 @@
   
   
   # preorder traverse of the tree and collect states
+  # It shouldne't be able to complete remove
   # input: tree, initial.state
   # output: a list, with tuples as elemetns, where each tuple is (treeNode, state)
   # find the state for the event point at the child end of the edge.index
@@ -131,15 +138,36 @@
     edges <- (1:nrow(tree$edge))[-c(edge.index:final.index)]
     pprint(edges)
     
+    # hack, temporaily change the rate matrix to remove the delte state
+    qTemp <- removeAbsorbingState(Q)
+  
     for (i in edges) {
-      result[tree$edge[i,2]] <- sampleCTMC(result[[tree$edge[i,1]]], tree$edge.length[i])
+      result[tree$edge[i,2]] <- sampleCTMC(result[[tree$edge[i,1]]], tree$edge.length[i], qTemp)
     }
-    
+        
     # simulate state for the event point
-    result[tree$edge[edge.index, 2]] <- sampleCTMC(result[[tree$edge[i,1]]], point)
+    result[tree$edge[edge.index, 2]] <- sampleCTMC(result[[tree$edge[i,1]]], point, qTemp)
     
     pprint(result)
     result
+  }
+  
+  # remove the absorbing
+  removeAbsorbingState <- function(rateMatrix) {
+    
+    for(i in seq(nrow(rateMatrix))) {
+      if (rateMatrix[i, i] == 0) {
+        for(j in seq(nrow(rateMatrix))) {
+          rateMatrix[j, i] <- 0
+        }
+      }
+    }
+    
+    for(i in seq(nrow(rateMatrix))) {
+      rateMatrix[i, i] <- -sum(rateMatrix[i, -c(i)])
+    }
+    
+    return(rateMatrix)
   }
   
   # choose the edge, then the point on the edge (is equal to the round-robin, one pass sampling)
@@ -172,16 +200,29 @@
       pprint(edgePoint)
       # set the state at the root (normal state for cancer)
       temp1 <- NULL
-      if (runAbove & !(edgePoint[1] == 1 & edgePoint[2] == 0)) {
+      pprint(runAbove)
+      pprint( !(edgePoint[1] == 1 & edgePoint[2] == 0 & !runBelow))
+      
+      if (runAbove & ( !(edgePoint[1] == 1 & edgePoint[2] == 0) | !runBelow) ) {
+        if (edgePoint[1] == 1 & edgePoint[2] == 0) {
+          edgePoint <- pickPointOnEdge(tree, INF)
+        }
         temp1 <- treeCTMC.edge.point.exclude(tree, 1, edgePoint[1], edgePoint[2])
         rareEventState <- temp1[tree$edge[edgePoint[1], 2]]
       } else {
         rareEventState <- sample(states, 1, prob=pi)
       }
-          
-      temp2 <- treeCTMC.edge.point(tree, rareEventState, edgePoint[1], edgePoint[2])
       
-      if (is.null(temp1)) temp1 <- temp2
+      pprint(temp1)
+      
+      temp2 <- NULL
+      if(runBelow)
+        temp2 <- treeCTMC.edge.point(tree, rareEventState, edgePoint[1], edgePoint[2])
+      
+      if (is.null(temp1)) 
+        temp1 <- temp2
+      else if (is.null(temp2))
+        temp2 <- temp1
       
       # merge the two
       sites[[i]] <- list()
@@ -237,6 +278,7 @@
     
     # SHOULD simulated above rare event?
     runAbove <<- TRUE
+    runBelow <<- TRUE
     
     #x <- xx.tree
     plot(x)
@@ -247,10 +289,49 @@
     
     state.labels <- c("A", "T", "C", "G", '-')
     states <- seq(state.labels)
-    mu <- .1
+    mu <- .01
     #rateMatrix <- matrix(c(-3, 1, 1, 1,   1, -3, 1, 1,   1, 1, -3, 1,    1, 1, 1, -3   ), 4, 4)
-    rateMatrix <- deleteRateMatrix(1, size = length(states))
+    rateMatrix <- deleteRateMatrix(mu, size = length(states))
     print(rateMatrix)
+    initial.distribution <- rep(1/length(states), length(states))
+    # put zero for the delete state
+    if (rateMatrix[length(states), length(states)] == 0) {
+      initial.distribution[length(states)] <- 0
+    }
+    
+    # (tree, nSites, initialDistribution, rateMatrix , states, state.labels, mu) {
+    driver(x, nSites, initial.distribution, rateMatrix, states, state.labels, mu)
+  }
+ 
+  
+  example.ctmc <- function() {
+    nSites <- 48
+    nTaxa <- 4
+    set.seed(4)
+    maxCopyNumber <- 3
+    x <- rtree(nTaxa,T)
+    x$tip.label <- paste0('t', 1:nTaxa)
+    
+    # SHOULD simulated above rare event?
+    runAbove <<- TRUE
+    runBelow <<- TRUE
+    
+    #x <- xx.tree
+    plot(x)
+    nodelabels()
+    tiplabels();
+    x <- reorder(x)
+    xx.tree <<- x
+    
+    rateMatrix.above <- generateCNRateMatrix.above(maxCopyNumber)
+    rateMatrix.below <- generateCNRateMatrix.below(maxCopyNumber)
+    
+    state.labels <- colnames(rateMatrix.above)
+    states <- seq(state.labels)
+    mu <- .01
+    print(rateMatrix.above)
+    print(rateMatrix.below)
+    
     initial.distribution <- rep(1/length(states), length(states))
     # put zero for the delete state
     if (rateMatrix[length(states), length(states)] == 0) {
@@ -279,6 +360,95 @@
     return(result)
   }
   
+  generateCNCTMCStateString <- function(i, j) {
+    paste0('(', i, ',', j, ')')
+  }
+  
+  stringToCTMCPair <- function(stateStr) {
+    #stateStr <- "(1 , 2)"
+    result <- gsub('\\(', '', stateStr)
+    result <- gsub('\\)', '', result)
+    result <- gsub(" ", "", result)
+    result <- strsplit(result, ",")
+    as.numeric(result[[1]])
+  }
+  
+  
+  # when a mutation hasn't happened
+  generateCNRateMatrix.above <- function(maxCN = 4) {
+    states <- c()
+    rateMatrix <- matrix(0, (maxCN + 1), (maxCN + 1))
+    
+    # here b corresponds to state -1, therefore there's no b in this simulator
+    for (i in 0:maxCN) { 
+      states <- c(states, generateCNCTMCStateString(i, 0))  
+    }
+  
+    for (i in seq(length(states))) {
+      pair <- stringToCTMCPair(states[i])
+      for (m in list(c(-1, 0), c(1, 0))) {
+        if (   (pair[1] + m[1]) >= 0 & 
+                 (pair[1] + m[1]) <= maxCN & 
+                 (pair[2] + m[2]) >= 0 & 
+                 (pair[2] + m[2]) <= maxCN) {
+          tempState <- generateCNCTMCStateString(pair[1] + m[1], pair[2] + m[2])
+          # prevent back mutation
+          if ( (pair[2] != 0 | m[2] == 0)  &  (pair[1] != 0 | m[1] == 0))
+            rateMatrix[i, grep(tempState, states)] <- 1
+        }
+      }
+    }
+    
+    # diagonal entries
+    for (i in seq(states))
+      rateMatrix[i, i] <- -sum(rateMatrix[i,])
+    
+    colnames(rateMatrix) <- states
+    rownames(rateMatrix) <- states
+    
+    rateMatrix
+  }
+  
+  # after a mutation (rare event occured)
+  generateCNRateMatrix.below <- function(maxCN = 4) {
+    states <- c()
+    rateMatrix <- matrix(0, (maxCN + 1)^2, (maxCN + 1)^2, byrow = TRUE)
+
+    # here b corresponds to state -1, therefore there's no b in this simulator
+    for (i in 0:maxCN) { 
+      for (j in 0:maxCN) { 
+        states <- c(states, generateCNCTMCStateString(i, j))  
+      }
+    }
+    
+    print(states)
+    
+    for (i in seq(length(states))) {
+      pair <- stringToCTMCPair(states[i])
+      print(pair)
+      for (m in list(c(-1, 0), c(1, 0), c(0, -1), c(0, 1))) {
+          if (   (pair[1] + m[1]) >= 0 & 
+                 (pair[1] + m[1]) <= maxCN & 
+                 (pair[2] + m[2]) >= 0 & 
+                 (pair[2] + m[2]) <= maxCN) {
+            tempState <- generateCNCTMCStateString(pair[1] + m[1], pair[2] + m[2])
+            # prevent back mutation
+            if ( (pair[2] != 0 | m[2] == 0)  &  (pair[1] != 0 | m[1] == 0))
+              rateMatrix[i, grep(tempState, states)] <- 1
+          }
+      }
+    }
+    
+    # diagonal entries
+    for (i in seq(states))
+      rateMatrix[i, i] <- -sum(rateMatrix[i,])
+    
+    colnames(rateMatrix) <- states
+    rownames(rateMatrix) <- states
+   
+    rateMatrix
+  }
+  
   pprint <- function(a) {
     if (shallPrint) {
       arg <- deparse(substitute(a))
@@ -296,6 +466,6 @@
     return(curr)
   }
   
-  ## 
+  #### Run examles 
 
-  example()
+  example.ctmc()
